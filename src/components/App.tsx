6e3d696152e9b3fd6532f2f64997964a32e2c721
twój login do GitHuba <twj email>
2023-05-20 15:51:17 +0200

    basic configuration

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Home } from "./Home"
+
+const App =()=> {
+
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () =>{
+    return(
+        <section>
+            <h1>PIZZA!</h1>
+        </section>
+    )
+}
+export {Home}
