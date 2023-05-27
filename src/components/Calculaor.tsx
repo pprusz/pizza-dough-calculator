771a87ea80f36dbb9fed67e49cda7c9008c60b00
twój login do GitHuba <twj email>
2023-05-27 22:13:13 +0200

    added sliders to changing ingredients value

--- src/components/Calculaor.tsx
+++ src/components/Calculaor.tsx
@@ -15,21 +15,30 @@ const Calculator: React.FC = () => {
     setInputs((prevInputs) => ({
       ...prevInputs,
       [id]: value,
+      
     }));
+    console.log(value)
   };
+  console.log('olej'+inputs.oil)
+
+  const handleSliderChange = (event: ChangeEvent<HTMLInputElement>) =>{
+    const value = event.target.value;
+    console.log(value)
+  }
 
   return (
     <section className="calculator">
       <h1 className="calculator_title">SKŁADNIKI</h1>
       <div className="calculator_ingr">
         <div className="calculator_ingr_element">
-          <label htmlFor="portion" className="calculator_ingr_element_title">
+          <label className="calculator_ingr_element_title">
             Ilość porcji
             <div className='tooltip'>
               <img src="/src/assets/question.png" alt=""/>
               <span>Podaj liczbę odpowiadającą ilości kulek na pizze, które chcesz uzyskać.</span>
             </div>
           </label>
+          <input onChange={handleInputChange} value={inputs.portion}  type="range" min="1" max="20" className="calculator_slider" id="portion"/>
           <input
             pattern="\d*"
             maxLength={2}
@@ -42,14 +51,14 @@ const Calculator: React.FC = () => {
           />
         </div>
         <div className="calculator_ingr_element">
-          <label htmlFor="weight" className="calculator_ingr_element_title">
+          <label className="calculator_ingr_element_title">
             Waga jednej porcji (gram)
             <div className='tooltip'>
               <img src="/src/assets/question.png" alt=""/>
               <span className='even'>Podaj wagę jaką mają mieć Twoje kulki na pizzę, zwykle wartość ta wynosi 250-300g zależnie od wielkość pizzy jaką chcesz uzyskać.</span>
             </div>
           </label>
-
+          <input onChange={handleInputChange} value={inputs.weight} step='10' type="range" min="150" max="350" className="calculator_slider" id="weight"/>
           <input
             pattern="\d*"
             maxLength={3}
@@ -62,13 +71,14 @@ const Calculator: React.FC = () => {
           />
         </div>
         <div className="calculator_ingr_element">
-          <label htmlFor="hydro" className="calculator_ingr_element_title">
+          <label className="calculator_ingr_element_title">
             Hydracja ciasta (%)
             <div className='tooltip'>
               <img src="/src/assets/question.png" alt=""/>
               <span>Ilość wody w cieście w stosunku do mąki. Przy okrągłych pizzach optymalną ilością jest zakres 55% – 65%.</span>
             </div>
           </label>
+          <input onChange={handleInputChange} value={inputs.hydro}  type="range" min="50" max="80" className="calculator_slider" id="hydro"/>
           <input
             pattern="\d*"
             maxLength={2}
@@ -81,13 +91,14 @@ const Calculator: React.FC = () => {
           />
         </div>
         <div className="calculator_ingr_element">
-          <label htmlFor="salt" className="calculator_ingr_element_title">
+          <label className="calculator_ingr_element_title">
             Sól na litr wody (gram)
             <div className='tooltip'>
               <img src="/src/assets/question.png" alt=""/>
-              <span className='even'>Wybierz ilość soli biorąc pod uwagę kilka wskazówek. Sugerowany zakres wynosi od 40 g – 60 g na litr wody.</span>
+              <span className='even'>Wybierz ilość soli biorąc pod uwagę kilka wskazówek. Sugerowany zakres wynosi od 40 g – 65 g na litr wody.</span>
             </div>
           </label>
+          <input onChange={handleInputChange} value={inputs.salt} step='5'  type="range" min="0" max="65" className="calculator_slider" id="salt"/>
           <input
             pattern="\d*"
             maxLength={2}
@@ -100,13 +111,14 @@ const Calculator: React.FC = () => {
           />
         </div>
         <div className="calculator_ingr_element">
-          <label htmlFor="oil" className="calculator_ingr_element_title">
+          <label className="calculator_ingr_element_title">
             Oliwa na litr wody (gram)
             <div className='tooltip'>
               <img src="/src/assets/question.png" alt=""/>
               <span>Podaj ilość tłuszczu jaką chcesz dodać do ciasta. Sugerowana ilość 30-50g na litr wody.</span>
             </div>
           </label>
+          <input onChange={handleInputChange} value={inputs.oil} step='5' type="range" min="0" max="50" className="calculator_slider" id="oil"/>
           <input
             pattern="\d*"
             maxLength={2}
@@ -119,7 +131,6 @@ const Calculator: React.FC = () => {
           />      
         </div>
       </div>
-
       <Result inputs={inputs} />
     </section>
   );
